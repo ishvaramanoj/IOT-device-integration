83c10b3a3658638535f864837e104940fde0429d
Ishvara <ishvaramanoj@gmail.com>
2024-03-17 15:05:38 +0530

    logoandslogan

--- IOT/src/App.jsx
+++ IOT/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './commonComponents/commonNavBar/navBar'
+
+
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     dddd
+
+     <NavBar/>
     </>
   )
 }
---
+++ IOT/src/commonComponents/commonNavBar/navBar.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import './navBar.css';
+
+
+const NavBar = ()=>{
+
+
+
+    return(
+        <>
+
+        <div className="navbar_body">
+
+        <div className="logo_body"></div>
+
+        <div className="project_name">
+            <div className="project_name_one">University Of</div>
+            <div className="project_name_two">Peradeniya</div>
+            <div className="slogan"><i>Connect.Create.Collaborate</i></div>
+        </div>
+
+        </div> 
+
+        </>
+    )
+}
+export default NavBar;
